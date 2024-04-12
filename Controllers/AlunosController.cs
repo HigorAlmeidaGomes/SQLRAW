ca955a2171230b8d5d0ab13b62b395d3b5349aa6
Higor Almeida Gomes <38301981+HigorAlmeidaGomes@users.noreply.github.com>
2024-04-12 09:53:32 -0300

    Inclusao do projeto para teste do para retorna um conjunto de resultados (IQueryable)

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLRAW.Context;
+using SQLRAW.Models;
+
+namespace SQLRAW.Controllers;
+public class AlunosController : Controller
+{
+    private readonly AppDbContext _appDbContext;
+
+    public AlunosController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var alunos = _appDbContext.Alunos.FromSql<Aluno>($@"SELECT
+                                          Alunos.AlunoId,
+                                          Alunos.Nome,
+                                          Alunos.Cidade,
+                                          Alunos.DataMatricula,
+                                          Alunos.Ativo,
+                                          Alunos.CursoId
+                                         FROM
+                                          Alunos");
+
+        var resultado = await alunos.ToListAsync();
+        return View(resultado);
+    }
+
+    public async Task<IActionResult> AlunosCursos(string cidade)
+    {
+        return View();
+    }
+}
---
+++ Models/AlunoCurso.cs
@@ -0,0 +1,11 @@
+﻿namespace SQLRAW.Models;
+
+public class AlunoCurso
+{
+    public string? NomeAluno { get; set; }
+    public string? CidadeAluno { get; set; }
+    public DateOnly? DataMatriculaAluno { get; set; }
+    public bool AlunoAtivo { get; set; }
+    public string? NomeCurso { get; set; }
+    public DateOnly? InicioCurso { get; set; }
+}
